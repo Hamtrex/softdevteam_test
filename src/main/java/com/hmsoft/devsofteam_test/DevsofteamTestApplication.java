7eeaea0f31c52f8b462b923a62b87352cdcb08f4
Hasson Medrano <hmedrano@universales.com>
2021-11-03 10:47:48 -0600

    Added Configuration

---
+++ src/main/java/com/hmsoft/devsofteam_test/DevsofteamTestApplication.java
@@ -0,0 +1,13 @@
+package com.hmsoft.devsofteam_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevsofteamTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevsofteamTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hmsoft/devsofteam_test/DevsofteamTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hmsoft.devsofteam_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevsofteamTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
